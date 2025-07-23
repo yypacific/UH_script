1bca642f9dc4ecb74aeb61146053e53fbc96d4b0
yypacific <ootori.hey.you9180@gmail.com>
2025-07-23 18:19:11 +0900

    add sam2bam

--- r1_read_alignment.sh
+++ r1_read_alignment.sh
@@ -7,10 +7,12 @@
 proj=~/UMHM_RNAseq
 sample=HM1first
 
-/home/soft/hisat2-2.1.0/hisat2 \
+hisat2 \
 	-p 4 \
 	-x ${proj}/reference_Ens/galgal5_hisat/galgal5_hs2 \
 	--dta-cufflinks \
 	-1 ${proj}/raw_data/${sample}/${sample}_1.fq.gz \
 	-2 ${proj}/raw_data/${sample}/${sample}_2.fq.gz \
 	-S ${proj}/hisat2_galgal5/${sample}_gal5.sam
+
+samtools sort -O bam -o ${sample}_sorted.bam ${sample}.sam
